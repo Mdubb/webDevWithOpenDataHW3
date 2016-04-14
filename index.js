9f46fa44beb84adb2c6a1bc380873af0b02c7f15
Michael Weber <Michael@172-16-254-59.DYNAPOOL.NYU.EDU>
2016-04-14 11:18:59 -0400

    hw 5

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ app.use(express.static('public'));
 app.listen(3000, function () {
 	console.log("Ice Cream is LIVE on port 3000.");
 
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
 
 });
